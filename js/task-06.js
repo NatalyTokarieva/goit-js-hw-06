a3e72a747cae1935c806525cdcf88b12eba5332c
Nataliia Tokarieva <tokareva_nata@ukr.net>
2022-07-28 23:40:32 +0200

    task  6, 7, 8 ,9

---
+++ js/task-06.js
@@ -0,0 +1,27 @@
+// // Напиши скрипт, который при потере фокуса на инпуте (событие blur), 
+// проверяет его содержимое на правильное количество введённых символов.
+
+// // <input
+// //   type="text"
+// //   id="validation-input"
+// //   data-length="6"
+// //   placeholder="Please enter 6 symbols"
+// // />
+// // Сколько символов должно быть в инпуте, указывается в его
+//  атрибуте data-length.
+// // Если введено подходящее количество символов, то border
+//  инпута становится зелёным, если неправильное - красным.
+// // Для добавления стилей, используй CSS-классы valid и invalid, 
+// которые мы уже добавили в исходные файлы задания.
+
+
+const  inputValid = document.querySelector('#validation-input');
+inputValid.addEventListener("blur", (event) =>{
+    if(inputValid.value.length !== Number(inputValid.dataset.length)){
+        inputValid.classList.add("invalid");
+    }else{
+        inputValid.classList.remove("invalid");
+        inputElement.classList.add("valid");
+    }
+});
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,27 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+// Напиши скрипт, который изменяет цвета фона элемента
+//  <body> через инлайн стиль при клике на 
+//   button.change-color и выводит значение цвета в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+
+
+const backgroundColor = document.querySelector('.color');
+const buttonColor = document.querySelector('.change-color');
+const widget = document.querySelector('.widget');
+
+backgroundColor.textContent = widget.style.backgroundColor;
+buttonColor.addEventListener('click', () => {
+document.body.style.backgroundColor = getRandomHexColor();
+});
+
+
+
